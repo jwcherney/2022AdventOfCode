488504e59f2321d7ae5fac4bb4bc19c0a110abbc
John Cherney <jcherney@ford.com>
2022-12-06 15:15:36 -0500

    Minor optimization

--- day06/day06.py
+++ day06/day06.py
@@ -5,14 +5,14 @@ TEST_DATAFILE = "test_input_data.txt"
 DATAFILE = REAL_DATAFILE
 
 
-START_OF_MARKER = 4
+START_OF_PACKET = 4
 START_OF_MESSAGE = 14
 
 
-def part1(s_line, buffer_length):
+def find_marker(s_line, marker_length):
     # print(f'line: {s_line}')
-    for i in range(buffer_length, len(s_line)):
-        marker = s_line[i-buffer_length:i]
+    for i in range(marker_length, len(s_line)):
+        marker = s_line[i - marker_length:i]
         # print(f'marker = {marker}')
         chars = list(marker)
         # print(f'chars = {chars}')
@@ -26,19 +26,14 @@ def part1(s_line, buffer_length):
     return -1
 
 
-def part2():
-    print("part 2 output")
-
-
 def day06(filename):
     with open(filename, 'r') as f:
         input_data = f.readlines()
     for line in input_data:
-        marker_index = part1(line.strip(), START_OF_MARKER)
-        message_index = part1(line.strip(), START_OF_MESSAGE)
+        marker_index = find_marker(line.strip(), START_OF_PACKET)
+        message_index = find_marker(line.strip(), START_OF_MESSAGE)
         print(f' Marker index: {marker_index}')
         print(f'Message index: {message_index}')
-    part2()
 
 
 if __name__ == '__main__':
