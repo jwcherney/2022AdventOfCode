0936bba2dbd49427159653b5fe552f61500ada9b
John Cherney <jcherney@ford.com>
2022-12-06 23:20:30 -0500

    Day02 optimizations

--- day02/day02.py
+++ day02/day02.py
@@ -9,78 +9,26 @@ WIN = 6
 TIE = 3
 LOSS = 0
 
-
-def part_one_rps_score(choices):
-    if choices[0] == "A":
-        if choices[1] == "X":
-            return_value = SCORE_R + TIE
-        elif choices[1] == "Y":
-            return_value = SCORE_P + WIN
-        elif choices[1] == "Z":
-            return_value = SCORE_S + LOSS
-        else:
-            return_value = 0
-    elif choices[0] == "B":
-        if choices[1] == "X":
-            return_value = SCORE_R + LOSS
-        elif choices[1] == "Y":
-            return_value = SCORE_P + TIE
-        elif choices[1] == "Z":
-            return_value = SCORE_S + WIN
-        else:
-            return_value = 0
-    elif choices[0] == "C":
-        if choices[1] == "X":
-            return_value = SCORE_R + WIN
-        elif choices[1] == "Y":
-            return_value = SCORE_P + LOSS
-        elif choices[1] == "Z":
-            return_value = SCORE_S + TIE
-        else:
-            return_value = 0
-    return return_value
-
-
-def part_two_rps_score(choices):
-    if choices[0] == "A":
-        if choices[1] == "X":
-            return_value = SCORE_S + LOSS
-        elif choices[1] == "Y":
-            return_value = SCORE_R + TIE
-        elif choices[1] == "Z":
-            return_value = SCORE_P + WIN
-        else:
-            return_value = 0
-    elif choices[0] == "B":
-        if choices[1] == "X":
-            return_value = SCORE_R + LOSS
-        elif choices[1] == "Y":
-            return_value = SCORE_P + TIE
-        elif choices[1] == "Z":
-            return_value = SCORE_S + WIN
-        else:
-            return_value = 0
-    elif choices[0] == "C":
-        if choices[1] == "X":
-            return_value = SCORE_P + LOSS
-        elif choices[1] == "Y":
-            return_value = SCORE_S + TIE
-        elif choices[1] == "Z":
-            return_value = SCORE_R + WIN
-        else:
-            return_value = 0
-    return return_value
+# ABC is row, XYZ is columns
+PART_1_SCORE = [[SCORE_R + TIE, SCORE_P + WIN, SCORE_S + LOSS],
+                [SCORE_R + LOSS, SCORE_P + TIE, SCORE_S + WIN],
+                [SCORE_R + WIN, SCORE_P + LOSS, SCORE_S + TIE]]
+PART_2_SCORE = [[SCORE_S + LOSS, SCORE_R + TIE, SCORE_P + WIN],
+                [SCORE_R + LOSS, SCORE_P + TIE, SCORE_S + WIN],
+                [SCORE_P + LOSS, SCORE_S + TIE, SCORE_R + WIN]]
 
 
 def day02(filename):
     with open(filename, 'r') as f:
-        input_data = f.readlines()
+        input_data = f.read().split('\n')
     p1_score = 0
     p2_score = 0
     for line in input_data:
-        line = line.strip()
-        p1_score += part_one_rps_score(line.split())
-        p2_score += part_two_rps_score(line.split())
+        values = line.split()
+        first = ord(values[0]) - ord('A')
+        second = ord(values[1]) - ord('X')
+        p1_score += PART_1_SCORE[first][second]
+        p2_score += PART_2_SCORE[first][second]
     print(f'Part 1 Score = {p1_score}')
     print(f'Part 2 Score = {p2_score}')
 
