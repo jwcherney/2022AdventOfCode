d53c5c4c574ca8688434ec5306923b125b34d40a
John Cherney <jcherney@ford.com>
2022-12-14 13:22:33 -0500

    Placeholders for the reminder of the days

---
+++ day25/day25.py
@@ -0,0 +1,17 @@
+REAL_DATAFILE = "input_data.txt"
+TEST_DATAFILE = "test_input_data.txt"
+
+# DATAFILE = REAL_DATAFILE
+DATAFILE = TEST_DATAFILE
+
+
+def day25(filename):
+    with open(filename, 'r') as f:
+        input_data = f.read().split('\n')
+    print(f'Input: {input_data}')
+    print(f'Part1: Not Implemented')
+    print(f'Part2: Not Implemented')
+
+
+if __name__ == '__main__':
+    day25(DATAFILE)
