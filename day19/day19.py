f425080624f7fd6401b2f700c5dafb2ee91a602a
John Cherney <jcherney@ford.com>
2022-12-19 12:46:37 -0500

    Day19, completely incomplete. Another DFS
    Will try copying known solution to get more experience w DFS type algorithms.

--- day19/day19.py
+++ day19/day19.py
@@ -5,11 +5,61 @@ TEST_DATAFILE = "test_input_data.txt"
 DATAFILE = TEST_DATAFILE
 
 
+ORE=0
+CLAY=1
+OBSIDIAN=2
+GEODE=3
+def part1(blueprints):
+    for blueprint in blueprints:
+        num, ore_ore_need, clay_ore_need, obsidian_ore_need, obsidian_clay_need, geode_ore_need, geode_obsidian_need = blueprint
+        factories = [0] * 4
+        commodities = [0] * 4
+        factories[0] = 1
+        for minute in range(1, 25):
+            new_factories = [0] * 4
+            if commodities[ORE] >= geode_ore_need and commodities[OBSIDIAN] >= geode_obsidian_need:
+                new_factories[GEODE] += 1
+                commodities[ORE] -= geode_ore_need
+                commodities[OBSIDIAN] -= geode_obsidian_need
+            elif commodities[ORE] >= obsidian_ore_need and commodities[CLAY] >= obsidian_clay_need and commodities[OBSIDIAN] + factories[OBSIDIAN] <= geode_obsidian_need:
+                new_factories[OBSIDIAN] += 1
+                commodities[ORE] -= obsidian_ore_need
+                commodities[CLAY] -= obsidian_clay_need
+            elif commodities[ORE] >= clay_ore_need and commodities[CLAY] + factories[CLAY] <= obsidian_clay_need:
+                new_factories[CLAY] += 1
+                commodities[ORE] -= clay_ore_need
+            for i in range(0, len(factories)):
+                commodities[i] += factories[i]
+                factories[i] += new_factories[i]
+            print(f'Minute {minute}')
+            print(f'  factories: {factories}')
+            print(f'  commodities: {commodities}')
+    return []
+
+
+def process_input(input_data):
+    blueprints = list()
+    for line in input_data:
+        data = line.split()
+        num = data[1].strip(':')
+        ore = int(data[6])
+        clay = int(data[12])
+        obsidian1 = int(data[18])
+        obsidian2 = int(data[21])
+        geode1 = int(data[27])
+        geode2 = int(data[30])
+        blueprints.append((num, ore, clay, obsidian1, obsidian2, geode1, geode2))
+    return blueprints
+
+
 def day19(filename):
     with open(filename, 'r') as f:
         input_data = f.read().split('\n')
     print(f'Input: {input_data}')
-    print(f'Part1: Not Implemented')
+    blueprints = process_input(input_data)
+    print(f'Blueprints: {blueprints}')
+    quality_level = part1(blueprints)
+    print(f'Part1: Total Quality: {sum(quality_level)}')
     print(f'Part2: Not Implemented')
 
 
