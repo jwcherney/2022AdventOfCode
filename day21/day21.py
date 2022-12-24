0a93c8cfbd08994f1330cd3bd70c01310383d322
John Cherney <jcherney@ford.com>
2022-12-24 01:40:46 -0500

    Try solving part2 with bisection method
    Gets an answer within 1, depending on which direction approached.
    
    Works find for full data. Off by one (too high) for test data

--- day21/day21.py
+++ day21/day21.py
@@ -29,43 +29,16 @@ class Monkey:
     def is_constant(self):
         return len(self.operation) == 0
 
-    def get_number(self, left_monkeys, right_monkeys=None, constant_monkeys=None, found_root=True):
-        if right_monkeys is None:
-            right_monkeys = dict()
-        if constant_monkeys is None:
-            constant_monkeys = dict()
+    def get_number(self, monkeys):
         if len(self.operation) == 0:
-            print(f'constant: {self} ({found_root})')
+            # print(f'constant: {self} ({found_root})')
             return self.num
         else:
-            print(f'Doing operation: {self} ({found_root})')
-            if found_root:
-                if self.m1name in constant_monkeys:
-                    lhs = constant_monkeys[self.m1name]
-                else:
-                    lhs = left_monkeys[self.m1name]
-                if self.m2name in constant_monkeys:
-                    rhs = constant_monkeys[self.m2name]
-                else:
-                    rhs = left_monkeys[self.m2name]
-            else:
-                if self.m1name == 'root':
-                    print(f'Found Root ({found_root})')
-                    return left_monkeys[self.m2name].get_number(left_monkeys, right_monkeys, constant_monkeys, True)
-                elif self.m2name == 'root':
-                    print(f'Found Root ({found_root})')
-                    return left_monkeys[self.m1name].get_number(left_monkeys, right_monkeys, constant_monkeys, True)
-                else:
-                    if self.m1name in constant_monkeys:
-                        lhs = constant_monkeys[self.m1name]
-                    else:
-                        lhs = right_monkeys[self.m1name]
-                    if self.m2name in constant_monkeys:
-                        rhs = constant_monkeys[self.m2name]
-                    else:
-                        rhs = right_monkeys[self.m2name]
-            return self.do_operation(lhs.get_number(left_monkeys, right_monkeys, constant_monkeys, found_root),
-                                     rhs.get_number(left_monkeys, right_monkeys, constant_monkeys, found_root))
+            # print(f'Doing operation: {self} ({found_root})')
+            lhs = monkeys[self.m1name]
+            rhs = monkeys[self.m2name]
+            return self.do_operation(lhs.get_number(monkeys),
+                                     rhs.get_number(monkeys))
 
     def do_operation(self, m1, m2):
         if self.operation == '+':
@@ -75,34 +48,13 @@ class Monkey:
         elif self.operation == '*':
             return m1 * m2
         elif self.operation == '/':
-            return m1 / m2
+            return m1 // m2
         else:
             print(f'BAD OPERATION DETECTED: {self.operation}')
             return 0
 
-    def get_lhs_monkey(self):
-        lhs = Monkey()
-        lhs.name = self.m1name
-        lhs.m1name = self.name
-        lhs.operation = get_opposite_operation(self.operation)
-        lhs.m2name = self.m2name
-        return lhs
-
-    def get_rhs_monkey(self):
-        rhs = Monkey()
-        rhs.name = self.m2name
-        if self.operation == '-' or self.operation == '/':
-            rhs.m1name = self.m1name
-            rhs.operation = self.operation
-            rhs.m2name = self.name
-        else:
-            rhs.m1name = self.name
-            rhs.operation = get_opposite_operation(self.operation)
-            rhs.m2name = self.m1name
-        return rhs
-
     def __str__(self):
-        if len(self.operation) == 0:
+        if self.is_constant():
             return self.name + ': ' + str(self.num)
         else:
             return self.name + ': ' + self.m1name + ' ' + self.operation + ' ' + self.m2name
@@ -113,43 +65,8 @@ def get_opposite_operation(operation):
     return output[['+', '-', '*', '/'].index(operation)]
 
 
-def solve_for_human(input_data):
-    constant_monkeys = dict()
-    left_monkeys = dict()
-    right_monkeys = dict()
-    for line in input_data:
-        cooked_monkey = Monkey(line)
-        if cooked_monkey.name == 'humn':
-            continue
-        if not cooked_monkey.is_constant():
-            # Make the lhs monkey and add
-            lhs_monkey = cooked_monkey.get_lhs_monkey()
-            if lhs_monkey.name not in right_monkeys:
-                right_monkeys[lhs_monkey.name] = lhs_monkey
-            else:
-                print(f'ERROR: LHS Monkey {lhs_monkey} already added')
-            # Make the rhs monkey and add
-            rhs_monkey = cooked_monkey.get_rhs_monkey()
-            if rhs_monkey.name not in right_monkeys:
-                right_monkeys[rhs_monkey.name] = rhs_monkey
-            else:
-                print(f'ERROR: RHS Monkey {rhs_monkey} already added')
-            # Handle adding the cooked_monkey
-            if cooked_monkey.name not in left_monkeys:
-                left_monkeys[cooked_monkey.name] = cooked_monkey
-            else:
-                print(f'ERROR: Cooked Monkey {cooked_monkey} already added')
-            # print(f'Monkey: {cooked_monkey}')
-            # print(f'LHS Monkey: {lhs_monkey}')
-            # print(f'RHS Monkey: {rhs_monkey}')
-        else:
-            constant_monkeys[cooked_monkey.name] = cooked_monkey
-            # print(f'Monkey: {cooked_monkey}')
-    return left_monkeys, right_monkeys, constant_monkeys
-
-
 def copied_solution_slightly_modified_for_my_code_thankyou_hyperneutrino(input_data):
-    monkeys = { "humn": sympy.Symbol("x") }
+    monkeys = {"humn": sympy.Symbol("x")}
     x = [line.strip() for line in input_data]
     ops = {
         "+": lambda x, y: x + y,
@@ -166,18 +83,60 @@ def copied_solution_slightly_modified_for_my_code_thankyou_hyperneutrino(input_d
             left, op, right = expr.split()
             if left in monkeys and right in monkeys:
                 if name == "root":
+                    # print(f' here1: {monkeys[name]}')
                     return sympy.solve(monkeys[left] - monkeys[right])[0]
                 monkeys[name] = ops[op](monkeys[left], monkeys[right])
             else:
                 x.append(a)
 
 
-def solve_for_root(input_data):
+def sign(n):
+    if n > 0:
+        return 1
+    elif n < 0:
+        return -1
+    else:
+        return 0
+
+
+def solve_for_human(monkeys):
+    human = monkeys['humn']
+    root = monkeys['root']
+    root.operation = get_opposite_operation(root.operation)
+    low_h = human.get_number(monkeys)
+    low_r = root.get_number(monkeys)
+    # print(f'Root when humn = {human.num} = {low_r}')
+    h_delta = 10
+    high_h = low_h * h_delta
+    human.num *= high_h
+    high_r = root.get_number(monkeys)
+    while sign(low_r) == sign(high_r):
+        high_h = high_h * 10
+        human.num = high_h
+        high_r = root.get_number(monkeys)
+    while not(abs(high_r - low_r) <= 1) and not(abs(high_h - low_h) <= 1):
+        mid_h = (high_h + low_h) // 2
+        human.num = mid_h
+        mid_r = root.get_number(monkeys)
+        if sign(mid_r) == sign(low_r):
+            low_h = mid_h
+            low_r = mid_r
+        else:
+            high_h = mid_h
+            high_r = mid_r
+        # print(f'({low_r}, {low_h}, {h_delta})')
+    if low_r == 0:
+        return low_h
+    else:
+        return high_h
+
+
+def make_monkeys(input_data):
     monkeys = dict()
     for line in input_data:
-        cooked_monkey = Monkey(line)
-        monkeys[cooked_monkey.name] = cooked_monkey
-        # print(f'Monkey: {cooked_monkey}')
+        monkey = Monkey(line)
+        monkeys[monkey.name] = monkey
+        # print(f'Monkey: {monkey}')
     return monkeys
 
 
@@ -185,14 +144,13 @@ def day21(filename):
     with open(filename, 'r') as f:
         input_data = f.read().split('\n')
     # print(f'Input: {input_data}')
-    monkeys = solve_for_root(input_data)
+    monkeys = make_monkeys(input_data)
     # print(f'Monkey Count: {len(monkeys)}')
     root_total = monkeys['root'].get_number(monkeys)
     print(f'Part1: Root Total = {root_total}')
-    left_monkeys, right_monkeys, constant_monkeys = solve_for_human(input_data)
-    # print(f'Monkey Count: {len(left_monkeys)}, {len(right_monkeys)}')
-    human_total = right_monkeys['humn'].get_number(left_monkeys, right_monkeys, constant_monkeys, False)
-    print(f'Part2: Human Total = {human_total}')
+    monkeys = make_monkeys(input_data)
+    human_total2 = solve_for_human(monkeys)
+    print(f'Part2: Human Total = {human_total2}')
     print(f'Part2: Human Total = {copied_solution_slightly_modified_for_my_code_thankyou_hyperneutrino(input_data)}')
 
 
